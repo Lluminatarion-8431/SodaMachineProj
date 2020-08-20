5a9d3a80f178322e154ca8d4a9b13e3fc4229703
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-08-20 13:20:19 -0700

    Created Member objects in Simulation for the payment choice, checking if there is enough funds in the card for the users selection of soda, and to give the user the ability to make another purchase according to their yes or no response.

--- Simulation.cs
+++ Simulation.cs
@@ -9,9 +9,94 @@ namespace SodaMachineProj
     class Simulation
     {
         //Member Variables (Has A)
-       
+        public SodaMachine sodaMachine;
+        public Customer customer;
+        bool sodaMachineChoice = true;
+
         //Constructor (Spawner)
+        public Simulation()
+        {
+            sodaMachine = new SodaMachine();
+            customer = new Customer();
+            UserInterface.Welcome();
+            string paymentChoice = UserInterface.ChoosePayment();
+            PaymentChoice(paymentChoice);
+        }
+
 
         //Member Methods (Can Do)
+        public void PaymentChoice(string paymentChoice)
+        {
+            while (sodaMachineChoice)
+            {
+                switch (paymentChoice) 
+                {
+                    case "1":
+                        CoinSodaMachineLoop();
+                        MakeAnotherPurchase();
+                        paymentChoice = UserInterface.ChoosePayment();
+                        break;
+                    case "2":
+                        CardSodaMachineLoop();
+                        MakeAnotherPurchase();
+                        paymentChoice = UserInterface.ChoosePayment();
+                        break;
+                    default:
+                        paymentChoice = UserInterface.ChoosePayment();
+                        break;
+                }
+            }
+        }
+        public void CardSodaMachineLoop()
+        {
+            bool inSodaMachineInventory = false;
+
+            UserInterface.CardBalance(customer.wallet.card);
+
+            while (!inSodaMachineInventory)
+            {
+                string sodaChoice = SodaSelection();
+                inSodaMachineInventory = sodaMachine.SodaInventory(sodaChoice);
+
+            }
+            CheckEnoughMoneyCard();
+        }
+
+        public bool MakeAnotherPurchase()
+        {
+            string anotherPurchase = UserInterface.AnotherPruchase().ToLower();
+            switch (anotherPurchase)
+            {
+                case "yes":
+                    sodaMachineChoice = true;
+                    break;
+                case "no":
+                    sodaMachineChoice = false;
+                default:
+                    sodaMachineChoice = false;
+                    break;
+            }
+            return sodaMachineChoice;
+        }
+        public void CheckEnoughMoney()
+        {
+            
+        }
+        public void SodaSelection()
+        {
+            
+        }
+        public void CheckEnoughMoneyCard()
+        {
+
+        }
+        public void AnotherPruchase()
+        {
+
+        }
+        public void CheckChangeAvailable()
+        {
+
+        }
     }
 }
